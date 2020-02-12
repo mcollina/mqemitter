867f9ee9e44f7bc183b733541d20178fae26ab61
gnought <1684105+gnought@users.noreply.github.com>
2020-02-12 17:15:11 +0800

    Add typings support

---
+++ mqemitter.d.ts
@@ -0,0 +1,24 @@
+/* eslint no-unused-vars: 0 */
+/* eslint no-undef: 0 */
+/* eslint space-infix-ops: 0 */
+
+/// <reference types="node" />
+
+interface MQEmitterOptions {
+  concurrency?: number
+  matchEmptyLevels?: boolean
+  separator?: string
+  wildcardOne?: string
+  wildcardSome?: string
+}
+
+export type Message = object & { topic: string }
+
+export interface MQEmitter {
+  emit(topic: string, callback: (error?: Error) => void): void
+  on(topic: string, listener: (message: Message, done: () => void) => void, callback?: () => void): void
+  removeListener(topic: string, listener: (message: Message, done: () => void) => void, callback?: () => void): void
+  close(callback: () => void): void
+}
+
+export function MQEmitter (options?: MQEmitterOptions): MQEmitter
---
+++ test/types/index.ts
@@ -0,0 +1,44 @@
+/* eslint no-unused-vars: 0 */
+/* eslint no-undef: 0 */
+
+import { MQEmitter, Message } from '../../mqemitter'
+
+const noop = function () {}
+
+var mq = MQEmitter()
+mq = MQEmitter({
+  concurrency: 100
+})
+mq.close(noop)
+
+mq = MQEmitter({
+  concurrency: 100,
+  matchEmptyLevels: true,
+  separator: ','
+})
+mq.close(noop)
+
+mq = MQEmitter({
+  concurrency: 10,
+  matchEmptyLevels: true,
+  separator: '/',
+  wildcardOne: '+',
+  wildcardSome: '#'
+})
+
+const notify = function (msg: Message, cb: () => void) {
+  if (msg.topic === 'hello/world') {
+    console.log(msg)
+  }
+  cb()
+}
+
+mq.on('hello/+', notify)
+
+mq.emit('hello/world', function (err) {
+  console.log(err)
+})
+
+mq.removeListener('hello/+', notify)
+
+mq.close(noop)
