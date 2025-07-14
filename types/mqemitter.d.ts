1a74a12e86d77e63b4f153e9cbb57dc4ff1eba56
Matteo Collina <hello@matteocollina.com>
2025-07-14 10:03:00 +0200

    Refactor types to use namespace
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- types/mqemitter.d.ts
+++ types/mqemitter.d.ts
@@ -8,15 +8,20 @@ interface MQEmitterOptions {
   wildcardSome?: string
 }
 
-export type Message = Record<string, any> & { topic: string }
+declare namespace mqemitter {
+  export type Message = Record<string, any> & { topic: string }
 
-export interface MQEmitter {
-  current: number
-  concurrent: number
-  on(topic: string, listener: (message: Message, done: () => void) => void, callback?: () => void): this
-  emit(message: Message, callback?: (error?: Error) => void): void
-  removeListener(topic: string, listener: (message: Message, done: () => void) => void, callback?: () => void): void
-  close(callback: () => void): void
+  export interface MQEmitter {
+    current: number
+    concurrent: number
+    on(topic: string, listener: (message: Message, done: () => void) => void, callback?: () => void): this
+    emit(message: Message, callback?: (error?: Error) => void): void
+    removeListener(topic: string, listener: (message: Message, done: () => void) => void, callback?: () => void): void
+    close(callback: () => void): void
+  }
+
 }
 
-export default function (options?: MQEmitterOptions): MQEmitter
+declare function mqemitter (options?: MQEmitterOptions): mqemitter.MQEmitter
+
+export = mqemitter
